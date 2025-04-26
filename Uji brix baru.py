afcacf38512d789eba721c7ac5a9f093c6343c00
KINGFelixBG <darisfadillah1140@gmail.com>
2025-04-26 19:01:06 +0700

    Update Uji brix baru.py

--- Uji brix baru.py
+++ Uji brix baru.py
@@ -42,10 +42,10 @@ if page == "Beranda":
     col1, col2 = st.columns(2)
 
     with col1:
-        st.image("https://i.ibb.co/QdJ6KHZ/pixel-scientist.png", caption="Steve - Peneliti Brix", use_column_width=True)
+        st.image("https://i.ibb.co/QdJ6KHZ/pixel-scientist.png", caption="Steve - Peneliti Brix", use_container_width=True)
 
     with col2:
-        st.image("https://i.ibb.co/whR1Zn7/pixel-fruit-man.png", caption="Creeper Buah - Pembawa Manis", use_column_width=True)
+        st.image("https://i.ibb.co/whR1Zn7/pixel-fruit-man.png", caption="Creeper Buah - Pembawa Manis", use_container_width=True)
 
     st.markdown("---")
 
