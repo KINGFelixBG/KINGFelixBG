8c4539928b81302a18b7faff82b1b7b85de5cceb
KINGFelixBG <darisfadillah1140@gmail.com>
2025-05-13 13:25:16 +0700

    Update Uji Brix pada pangan.py

--- Uji Brix pada pangan.py
+++ Uji Brix pada pangan.py
@@ -1,5 +1,16 @@
 import streamlit as st
-
+st.markdown("""
+    <style>
+    .stApp {
+        background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)),
+                    url('https://img.freepik.com/free-photo/person-paying-using-nfc-technology_23-2149893740.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        color: white !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 st.set_page_config(page_title="Uji Brix pada Bahan Pangan", layout="centered")
 
 # Title
