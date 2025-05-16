852e103531507fa2ca0eaf7f2e8659995da7ce3a
KINGFelixBG <darisfadillah1140@gmail.com>
2025-05-16 09:20:16 +0700

    Update Uji Brix pada pangan.py

--- Uji Brix pada pangan.py
+++ Uji Brix pada pangan.py
@@ -8,7 +8,7 @@ st.markdown("""
     <style>
     .stApp {
         background: linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)),
-                    url("https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHdoaXRlJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D");
+                    url("https://images.unsplash.com/photo-1724405143873-cdaa5cac918e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bGlnaHQlMjBibHVlJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D");
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
